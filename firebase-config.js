25c672db26b6b92ce1d39331c7111eec1d2b61d5
suttichai02 <suttichai021202@gmail.com>
2025-06-17 03:09:35 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -2,14 +2,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAmdR9VT1fplzsfXfeAe9eHdYF2Uv9o-Y",
-  authDomain: "photo-gallery-e2dec.firebaseapp.com",
-  projectId: "photo-gallery-e2dec",
-  storageBucket: "photo-gallery-e2dec.appspot.com",
+  apiKey: "AIzaSyAadMR9TVflpHzsfXIeAe9eHdVF2UwyO-Y",
+  authDomain: "photo-gallery-a2dec.firebaseapp.com",
+  projectId: "photo-gallery-a2dec",
+  storageBucket: "photo-gallery-a2dec.firebasestorage.app",
   messagingSenderId: "163307931322",
   appId: "1:163307931322:web:90e290b72543fd8ab92483",
-  measurementId: "G-ZB2RDRQKRR"
+  measurementId: "G-Z002RQDKRR"
 };
 
 const app = initializeApp(firebaseConfig);
